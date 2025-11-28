aaeb006a84172345f75b28a2138beee1ce02d391
danyalfun <danyal.fun@gmail.com>
2025-11-28 12:30:36 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -24,8 +24,8 @@ namespace ConsoleApp3
             int num;
             Console.WriteLine("ENTER NUMBER");
             num = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= num; i+=2)
-            { 
+            for (int i = 1; i <= num; i += 2)
+            {
                 Console.Write(i);
             }
 
@@ -51,11 +51,11 @@ namespace ConsoleApp3
             Console.WriteLine("enter days");
             days = int.Parse(Console.ReadLine());
             for (int i = 1; i <= days; i++)
-            { 
+            {
                 Console.WriteLine("step per day" + i);
                 dayStaps = int.Parse(Console.ReadLine());
                 if (dayStaps >= 10000)
-                { 
+                {
                     Console.WriteLine("YOU DO MORE 10000 STEPS THIS DAY CONGRADS");
                     OKSTEPSsteps = OKSTEPSsteps + 1;
                 }
@@ -64,12 +64,45 @@ namespace ConsoleApp3
             Console.WriteLine("sam steps =" + stepsCount);
             Console.WriteLine("YOU HAVE GOOD STEPS IN " + OKSTEPSsteps + " DAYS ");
         }
+        static void Task5()
+        {
+            int kids;
+            int samresults = 0;
+            int average = 0;
+            int passkids = 0;
+            int maxresult = 0;
+            int kidResult = 0;
+            Console.WriteLine("enter number of kids");
+            kids = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= kids; i++)
+            {
+                Console.WriteLine("kid " + i + " result");
+                kidResult = int.Parse(Console.ReadLine());
+                samresults = samresults + kidResult;
+                if (kidResult >= 55)
+                {
+                    Console.WriteLine("kid " + i + " good reasult");
+                    passkids = passkids + 1;
+                }
+                if (kidResult > maxresult)
+                {
+                    maxresult = kidResult;
+                }
+                
+            }
+            average = samresults / kids;
+            Console.WriteLine("average result = " + average);
+            Console.WriteLine("number of pass kids = " + passkids);
+            Console.WriteLine("max result = " + maxresult);
+
+        }
         static void Main(string[] args)
         {
             //Task1();
             //Task2();
             //Task3();
-            Task4();
+            //Task4();
+            Task5();
         }
     }
 }
